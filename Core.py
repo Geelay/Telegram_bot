1981cb474cc230fe1c7d2031ee6eefd8673d4a66
Geelay <gleb.kuz@bk.com>
2019-05-10 18:34:08 +0300

    first and second bots

---
+++ Core.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import telebot
+
+bot = telebot.TeleBot('814941567:AAGu3mHvWgCfqDU1BzzpSg5UEWhVAiKV9D0')
+
+keyboard1 = telebot.types.ReplyKeyboardMarkup()
+keyboard1.row('Привет', 'Пока')
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Привет, ты написал мне /start', reply_markup=keyboard1)
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    elif message.text.lower() == 'я тебя люблю':
+        bot.send_sticker(message.chat.id, 'CAADAgADZgkAAnlc4gmfCor5YbYYRAI')
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_id(message):
+    print(message)
+
+@bot.message_handler(commands=['create_account'])
+def create_account(message):
+    print('Здравствуй путник, назови своё имя')
+    
+
+bot.polling()
